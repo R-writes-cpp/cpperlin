6dbb424b2a3cc54f4635dc3949009831147aed67
R Alam <159417475+R-writes-cpp@users.noreply.github.com>
2024-04-17 21:44:01 +0100

    Minor source code updates
    - Changed many variables to use direct list initialisation
    - Set size_t types to be passed by value in various functions

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,10 @@ typedef unsigned char channel; // using unsigned chars to store integers in the 
 
 struct vec_2d // a simple 2D vector struct which we'll require for the Perlin noise algorithm
 {
-   double v1, v2;
+   double v1 {}, v2 {};
 
    vec_2d (double x, double y) : v1 {x}, v2 {y} {}
-   vec_2d () : v1 {0}, v2 {0} {}
+   vec_2d () {}
    double operator * (vec_2d& b)
    {
       return v1 * b.v1 + v2 * b.v2;
@@ -21,26 +21,26 @@ struct vec_2d // a simple 2D vector struct which we'll require for the Perlin no
 
 class grid_2d {
    public:
-      grid_2d (size_t& width, char* custom_seed) // this constructor first initialises the grid and the grid side length, then populates the grid with random unit vectors
+      grid_2d (size_t width, char* custom_seed) // this constructor first initialises the grid and the grid side length, then populates the grid with random unit vectors
          : grid_side {width / GRID_SCALE + 1},
            grid {vector<vector<vec_2d>> (ceil (grid_side), vector<vec_2d>(ceil (grid_side)))}
       {
          mt19937 mt([custom_seed]() { // using a lambda to return the value of the seed during construction
             if (custom_seed == nullptr) {
-               auto ret = []() {
+               auto ret {[]() {
                   random_device rd;
                   return rd();
-               }();
+               }()};
 
                cout << "Your seed is: " << ret << '\n';
                return ret;
             } else
-               return static_cast<unsigned int>(strtoul (custom_seed, nullptr, 10));
+               return static_cast<unsigned int>(atoi (custom_seed));
          }());
 
          uniform_real_distribution<double> dist(-1.0, 1.0);
          auto random_bits {mt()}; // stores a random output from the mersenne twister. during the construction of the grid, the bits of this output are used to determine whether each vector will have a positive or negative v2 value
-         int counter = 0;
+         int counter {0};
 
          for (auto& r : grid)
             for (auto& v : r) {
@@ -48,7 +48,7 @@ class grid_2d {
                   random_bits = mt(), // ...then generate some more in the form of a different number
                   counter = 0;
 
-               auto v1 = dist(mt), v2 = (random_bits & 1 ? 1 : -1) * sqrt(1 - v1 * v1); // after selecting a random number in the distribution (v1), we calculate the value v2 such that v1^2 + v2^2 = 1, so that v is a unit vector. after rearranging for v2, we find that v2 = sqrt(1 - v1^2). note that we multiply v by either 1 or -1, depending on the value of the rightmost bit of random_bits so that we have an even chance of v2 being either positive or negative - if we didn't do then then v2 would always be non-negative because 0 <= v1 * v1 <= 1 and hence 0 <= 1 - v1 * v1 <= 1
+               auto v1 {dist(mt)}, v2 {(random_bits & 1 ? 1 : -1) * sqrt(1 - v1 * v1)}; // after selecting a random number in the distribution (v1), we calculate the value v2 such that v1^2 + v2^2 = 1, so that v is a unit vector. after rearranging for v2, we find that v2 = sqrt(1 - v1^2). note that we multiply v by either 1 or -1, depending on the value of the rightmost bit of random_bits so that we have an even chance of v2 being either positive or negative - if we didn't do then then v2 would always be non-negative because 0 <= v1 * v1 <= 1 and hence 0 <= 1 - v1 * v1 <= 1
 
                v = vec_2d {std::move(v1), std::move(v2)}; // initialising v2 beforehand instead of inputting it as an rvalue allows us to move v1 (if we didn't do this then v1 would be used in both parameters and since C++23 has no guarantee on the order in which parameters are initialised, we would not be able to safely move v1)
 
@@ -57,12 +57,12 @@ class grid_2d {
             }
       }
 
-      vector<channel> get_img_vec(size_t& width)
+      vector<channel> get_img_vec(size_t width)
       {
          vector<channel> img;
          img.reserve(width * width);
-         for (double r = POINT_INTERVAL; r < grid_side - 1; r += POINT_INTERVAL)
-            for (double c = POINT_INTERVAL; c < grid_side - 1; c += POINT_INTERVAL)
+         for (double r {POINT_INTERVAL}; r < grid_side - 1; r += POINT_INTERVAL)
+            for (double c {POINT_INTERVAL}; c < grid_side - 1; c += POINT_INTERVAL)
                img.emplace_back (to_greyscale (get_coord_val(r, c))); // pushing the pixel at the coordinate (r, c) to the image. using emplace_back because to_greyscale returns a double
 
          return img;
@@ -78,8 +78,8 @@ class grid_2d {
 
       double get_coord_val (double r, double c)
       {
-         auto r_floor = floor(r), c_floor = floor(c);
-         auto r_floor_dist = r - r_floor, c_floor_dist = c - c_floor;
+         auto r_floor {floor(r)}, c_floor {floor(c)};
+         auto r_floor_dist {r - r_floor}, c_floor_dist {c - c_floor};
 
          // the distance vectors. these macros avoid the initialisation of unnecessary lvalues and move semantics when we only need rvalues for our interpolations
          #define top_left_dist     vec_2d (-r_floor_dist, c_floor_dist)
@@ -116,7 +116,7 @@ int main(int argc, char** argv)
    }
 
    size_t width = [argv]() { // a simple lambda to check whether there were issues with parsing the input dimensions with strtoul prior to initialising the width of the image
-      auto ret = strtoul (argv[1], nullptr, 10);
+      auto ret {strtoul (argv[1], nullptr, 10)};
       if (ret == 0) {
          cerr << "Error while parsing input dimension: positive integer not entered.\n";
          exit(1);
